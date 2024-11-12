6e52cdc6f3c9ca1e945534b86877fe9c637bfa2f
iurii <immirg@gmail.com>
2024-11-12 21:48:03 +0200

    lesson_15_fix

--- lesson_15/homework_15.py
+++ lesson_15/homework_15.py
@@ -1,40 +1,39 @@
 class Rhombus:
     name = 'Rhombus'
 
-    def __init__(self, side_a: int, angle_a: int, angle_b=None):
+    def __init__(self, side_a: int, angle_a: int = None, angle_b: int = None):
         self.side_a = side_a
-        self.angle_a = angle_a
-        self.angle_b = angle_b
+        self.angle_a = 180 - angle_b if angle_a is None else angle_a
+        self.angle_b = 180 - angle_a if angle_b is None else angle_b
 
     def __setattr__(self, key, value):
         if key == 'side_a':
-            try:
-                value = float(value)
-                if value <= 0:
-                    return 'Сторона А должна быть больше 0'
-            except ValueError:
-                return 'Сторона А не является числом'
+            if not isinstance(value, (int, float)) or value <= 0:
+                raise ValueError('Не корректно задана сторона А')
 
-        if key == 'angle_a':
-            try:
-                value = float(value)
-                if not (0 < value < 180):
-                    return 'Угол А должен быть больше 0 и меньше 180'
-            except ValueError:
-                return 'Угол А не является числом'
+        if key == 'angle_a' or key == 'angle_b':
+            if not isinstance(value, (int, float)) or not (0 < value < 180):
+                raise ValueError('Не корректно задан один из уголов')
+            if key == 'angle_a':
+                super().__setattr__('angle_b', 180 - value)
+            else:
+                super().__setattr__('angle_a', 180 - value)
 
         if key == 'angle_b':
             if 'angle_a' in self.__dict__ and 'angle_b' in self.__dict__ and self.angle_a + value != 180:
-                print(f'Значение угла B {value}° некорректно. Угол B установлен как {180 - self.angle_a}°')
-            value = 180 - self.angle_a
+                value = 180 - self.angle_a
         super().__setattr__(key, value)
 
     def __str__(self):
-        if 'side_a' in self.__dict__ and 'angle_b' in self.__dict__ and 'angle_b' in self.__dict__:
-            return f'{self.name}: side_a: {self.side_a}, angle_a: {self.angle_a}, angle_b: {self.angle_b}'
-        else:
-            return 'Присутствует ошбика в заданных параметрах'
+        return f'{self.name}: side_a: {self.side_a}, angle_a: {self.angle_a}, angle_b: {self.angle_b}'
 
 
-rhombus = Rhombus(side_a=4, angle_a=24)
+rhombus = Rhombus(side_a=4, angle_b=18)
 print(rhombus)
+print('*' * 30)
+rhombus.angle_a = 33
+print(rhombus)
+print('*' * 30)
+rhombus.angle_b = 111
+print(rhombus)
+
